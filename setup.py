3fe5e3c7736f4388a0da0ed0239245aceb34f4a8
Seon Kim <skim@signalfx.com>
2019-04-16 12:18:56 -0400

    Update dist

--- setup.py
+++ setup.py
@@ -3,42 +3,32 @@
 
 # Copyright (C) 2019 SignalFx, Inc. All rights reserved.
 
-import sys
 from setuptools import setup, find_packages
 
-if '--module' in sys.argv:
-    idx = sys.argv.index('--module')
-    sys.argv.pop(idx)
-    module = sys.argv.pop(idx)
+with open('signalfx_gcf/version.py') as f:
+    exec(f.read())
 
-    vars = {}
+with open('README.rst') as readme:
+    long_description = readme.read()
 
-    with open(module + '/version.py') as f:
-        exec(f.read(), vars)
+with open('requirements.txt') as f:
+    requirements = [line.strip() for line in f.readlines()]
 
-    with open('README.rst') as readme:
-        long_description = readme.read()
-
-    with open('requirements.txt') as f:
-        requirements = [line.strip() for line in f.readlines()]
-
-    setup(
-        name=vars['name'],  # noqa
-        version=vars['version'],  # noqa
-        author='SignalFx, Inc',
-        author_email='info@signalfx.com',
-        description='SignalFx Python Serverless Wrapper',
-        license='Apache Software License v2',
-        long_description=long_description,
-        long_description_content_type='text/x-rst',
-        zip_safe=True,
-        packages=vars['packages'],
-        install_requires=requirements,
-        classifiers=[
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-        ],
-        url='https://github.com/seonsfx/serverless-python',
-    )
-else:
-    print("Missing parameter : --module")
+setup(
+    name=name,  # noqa
+    version=version,  # noqa
+    author='SignalFx, Inc',
+    author_email='info@signalfx.com',
+    description='SignalFx Python Serverless Wrapper',
+    license='Apache Software License v2',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    zip_safe=True,
+    packages=['signalfx_gcf', 'signalfx_gcf.serverless'],
+    install_requires=requirements,
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    url='https://github.com/seonsfx/serverless-python',
+)
